a5cdeb1c800fbf5be545e20eb89f12c08b5609e9
coffeykaceyj <119899031+coffeykaceyj@users.noreply.github.com>
2022-12-08 15:59:04 -0500

    Update part1.py

--- Dec8/part1.py
+++ Dec8/part1.py
@@ -12,24 +12,19 @@ def is_visible_in_row(forest, row_index, column_index):
     tree = forest[row_index][column_index]
     left_side = forest[row_index][:column_index]
     right_side = forest[row_index][column_index + 1:]
-    
+    if len(right_side) == 0 or len(left_side) == 0:
+        return 1
     if tree > max(left_side) or tree > max(right_side):
         return 1
 
     return 0
 
 def tree_visibility_score(row_index, column_index):
-    if row_index == 0 or column_index == 0:
-        return 1
-    if row_index == len(forest_heights) - 1 or column_index == len(forest_heights[0]) - 1:
-        return 1
-    
     return is_visible_in_row(forest_heights, row_index, column_index) or is_visible_in_row(forest_heights_by_column, column_index, row_index)
 
 
 for line in input:
     row = [int(i) for i in line]
-    visibility_row = [False for i in line]
     forest_heights.append(row)
 
 for i in range(len(forest_heights[0])):
